2ee4680ba5b4a48f1b658b38a148f91f54f241d2
Jose David David <94769787+josexxda@users.noreply.github.com>
2022-05-01 19:35:51 +0000

    ahora le meti mas cositas

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-Salario_bruto= float(input("Ingrese tu salario: "))
+salario_bruto= float(input("Ingrese tu salario anual: "))
 
-if Salario_bruto>=0.01 and 100000.00 :
+if salario_bruto>=0.01 and 100000.00 :
         Impuesto_base1= 0.00
-        Porcentaje_aplicable= 
-        saldo_neto=subsaldo-saldo_alto
+        sobrexceso1=0.00
+        saldo_neto= salario_bruto-Impuesto_base1-sobrexceso1
         print("salario luego de deducciones: ", saldo_neto)
-        print("Deducciones inss  : ", saldo_alto)
-
-if subsaldo<=14999:
-        impuestos_bajos=0.08
-        saldo_bajo=subsaldo*impuestos_bajos
-        saldo_neto=subsaldo-saldo_bajo
-        print("salario luego de deducciones: ", saldo_neto)
-        print("salrio bajo :", saldo_bajo)
+  if salario_bruto>=
