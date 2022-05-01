c20c62a562ef33009da4c26c375e1f00992304fd
Jose David David <94769787+josexxda@users.noreply.github.com>
2022-05-01 20:42:15 +0000

    ahora si

--- main.py
+++ main.py
@@ -5,10 +5,4 @@ if salario_bruto<=100000.00 :
         sobrexceso1=0.00
         saldo_neto= salario_bruto-Impuesto_base1-sobrexceso1
         print("salario luego de deducciones: ", saldo_neto)
-  if salario_bruto>= 100001 and <= 200000.00:
-    if salario_bruto<=100000.00 :
-        Impuesto_base1= 0.00
-        sobrexceso1=0.00
-        saldo_neto= salario_bruto-Impuesto_base1-sobrexceso1
-        print("salario luego de deducciones: ", saldo_neto)    
-    
+  if salario_bruto>= 100001 and =< 200000.00:
