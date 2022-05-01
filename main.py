15de35a97aee9875a272915fe9db79a92c4309a1
Jose David David <94769787+josexxda@users.noreply.github.com>
2022-05-01 20:23:14 +0000

    ME FALTA LOS SALARIOS DE 100001 A 200000 POR PEDO DE INDENTACION

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 salario_bruto= float(input("Ingrese tu salario anual: "))
 
-if salario_bruto>=0.01 and 100000.00 :
+if salario_bruto<=100000.00 :
         Impuesto_base1= 0.00
         sobrexceso1=0.00
         saldo_neto= salario_bruto-Impuesto_base1-sobrexceso1
         print("salario luego de deducciones: ", saldo_neto)
-  if salario_bruto>=
+  if salario_bruto>= 100001 and <= 200000.00:
+    if salario_bruto<=100000.00 :
+        Impuesto_base1= 0.00
+        sobrexceso1=0.00
+        saldo_neto= salario_bruto-Impuesto_base1-sobrexceso1
+        print("salario luego de deducciones: ", saldo_neto)    
+    
