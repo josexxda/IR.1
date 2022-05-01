09b4d1c2743cfe8f60b415018f3502421785b50f
Jose David David <94769787+josexxda@users.noreply.github.com>
2022-05-01 21:14:40 +0000

    va  que quiere

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 salario_bruto= float(input("Ingrese tu salario anual: "))
 
-if salario_bruto<=100000.00 :
+if salario_bruto <=100000.00 :
         Impuesto_base1= 0.00
         sobrexceso1=0.00
         saldo_neto= salario_bruto-Impuesto_base1-sobrexceso1
         print("salario luego de deducciones: ", saldo_neto)
-  if salario_bruto>= 100001 and =< 200000.00:
+else : 
+  print("no se puede")
+if salario_bruto <=100001 and 200000:
+  Impuesto_base2 = 
