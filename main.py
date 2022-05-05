147c9db8a3ac8db0b2046416492faa80d6f66186
Jose David David <94769787+josexxda@users.noreply.github.com>
2022-05-05 18:26:58 +0000

    trapito

--- main.py
+++ main.py
@@ -1,46 +1,43 @@
-salario_bruto= int(input("Ingrese tu salario: "))*12
-print ("salario anual es :", salario_bruto)
-inss=salario_bruto*0.0625
+salario= int(input("Ingrese tu salario: "))*12
+print ("salario anual es :", salario)
+inss=salario*0.0625 /12
 print("su insss es :", inss )
-if salario_bruto <=100000.00 :
-  saldo_neto= ((salario_bruto-0.00)-0.00) 
+if salario <=100000.00 :
+  saldo_neto= ((salario-0.00)-0.00)
   print("salario luego de deducciones: ", saldo_neto)
   
-elif salario_bruto>=100000.01 and salario_bruto <= 200000.00:
-  deducciones_anuales=((salario_bruto-100000.00 )*0.15)+0.00
+elif salario>=100000.01 and salario<= 200000.00:
+  deducciones_anuales=((salario-100000.00 )*0.15)+0.00
   deducciones_mensual=deducciones_anuales/12
-  saldo_neto2=(salario_bruto-deducciones_anuales) / 12 
+  saldo_neto2=(salario-deducciones_anuales) / 12
   print("salario  mensual luego de deducciones: ", saldo_neto2)
-  print("las deducciones anuales son  : ", deducciones_anuales)
-  print("IR mensual es: " , deducciones_mensual)
+  print("IR anual  : ", deducciones_anuales)
+  print("IR mensual : " , deducciones_mensual)
   
-elif salario_bruto>=200000.01 and salario_bruto <= 350000.00:
-  deducciones_anuales3=((salario_bruto-200000.00 )*0.20)+15000.00
+elif salario>=200000.01 and salario <= 350000.00:
+  deducciones_anuales3=((salario-200000.00 )*0.20)+15000.00
   deducciones_mensual_3=deducciones_anuales3/12
-  saldo_neto3=(salario_bruto-deducciones_anuales3) / 12 
+  saldo_neto3=(salario-deducciones_anuales3) / 12 
   print("salario  mensual luego de deducciones: ", saldo_neto3)
-  print("las deducciones anuales son  : ", deducciones_anuales3)
+  print("IR anual  : ", deducciones_anuales3)
   print("IR mensual es: " , deducciones_mensual_3)
   
-elif salario_bruto>=350000.01 and salario_bruto <= 500000.00:
-  deducciones_anuales4=((salario_bruto-350000.00 )*0.25)+45000
+elif salario>=350000.01 and salario <= 500000.00:
+  deducciones_anuales4=((salario-350000.00 )*0.25)+45000
   deducciones_mensual_4=deducciones_anuales4/12
-  saldo_neto4=(salario_bruto-deducciones_anuales4) / 12 
+  saldo_neto4=(salario-deducciones_anuales4) / 12 
   print("salario  mensual luego de deducciones: ", saldo_neto4)
-  print("las deducciones anuales son  : ", deducciones_anuales4)
+  print("IR anual  : ", deducciones_anuales4)
   print("IR mensual es: " , deducciones_mensual_4)
 
-elif salario_bruto>=500000.01 :
-  deducciones_anuales5=((salario_bruto-500000.00 )*0.30)+82500
+elif salario>=500000.01 :
+  deducciones_anuales5=((salario-500000.00 )*0.30)+82500
   deducciones_mensual_5=deducciones_anuales5/12
-  saldo_neto5=(salario_bruto-deducciones_anuales5) / 12 
+  saldo_neto5=(salario-deducciones_anuales5) / 12 
   print("salario  mensual luego de deducciones: ", saldo_neto5)
-  print("las deducciones anuales son  : ", deducciones_anuales5)
+  print("IR anual  : ", deducciones_anuales5)
   print("IR mensual es: " , deducciones_mensual_5)
 else:
   ("nel perrro")
   
   print("gracias")
-  #no se que mas hacer
-  #faltan crear funciones para las deducciones y inss
-  #quiero crear  una app para planilla
