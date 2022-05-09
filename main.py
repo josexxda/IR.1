fdbddf893a31bd9111727ea835b43affdcd26f6d
Jose David David <94769787+josexxda@users.noreply.github.com>
2022-05-09 22:04:51 +0000

    creo que lo idente

--- main.py
+++ main.py
@@ -1,43 +1,33 @@
-salario= int(input("Ingrese tu salario: "))*12
-print ("salario anual es :", salario)
-inss=salario*0.0625 /12
+#CALCULADORA DE IR Y INSS ANUAL
+salario =int(input("Ingrese tu salario: "))*12
+inss =salario*0.0625
 print("su insss es :", inss )
+#SALARIO ANUAL DE 100000.00 
 if salario <=100000.00 :
-  saldo_neto= ((salario-0.00)-0.00)
-  print("salario luego de deducciones: ", saldo_neto)
+     saldo_neto = ((salario-0.00)-0.00)
+     print("salario excento de IR: ", saldo_neto ,)
   
+#SALRIO ANUAL DE 100000.01 A 200000.00
 elif salario>=100000.01 and salario<= 200000.00:
-  deducciones_anuales=((salario-100000.00 )*0.15)+0.00
-  deducciones_mensual=deducciones_anuales/12
-  saldo_neto2=(salario-deducciones_anuales) / 12
-  print("salario  mensual luego de deducciones: ", saldo_neto2)
-  print("IR anual  : ", deducciones_anuales)
-  print("IR mensual : " , deducciones_mensual)
+     deducciones_anuales =((salario-100000.00 )*0.15)+0.00
+     print("IR anual  : ", deducciones_anuales)
   
+ #SALARIO DE 200000.01 A 350000.00
 elif salario>=200000.01 and salario <= 350000.00:
-  deducciones_anuales3=((salario-200000.00 )*0.20)+15000.00
-  deducciones_mensual_3=deducciones_anuales3/12
-  saldo_neto3=(salario-deducciones_anuales3) / 12 
-  print("salario  mensual luego de deducciones: ", saldo_neto3)
-  print("IR anual  : ", deducciones_anuales3)
-  print("IR mensual es: " , deducciones_mensual_3)
+     deducciones_anuales3 =((salario-200000.00 )*0.20)+15000.00
+     print("IR anual : ", deducciones_anuales3)
   
+#SALARIO ANUAL 350000.01 A 500000.00
 elif salario>=350000.01 and salario <= 500000.00:
-  deducciones_anuales4=((salario-350000.00 )*0.25)+45000
-  deducciones_mensual_4=deducciones_anuales4/12
-  saldo_neto4=(salario-deducciones_anuales4) / 12 
-  print("salario  mensual luego de deducciones: ", saldo_neto4)
-  print("IR anual  : ", deducciones_anuales4)
-  print("IR mensual es: " , deducciones_mensual_4)
-
+     deducciones_anuales4 =((salario-350000.00 )*0.25)+45000
+     print("IR anual  : ", deducciones_anuales4)
+  
+#SALARIO ANUAL 500000.01 A MAS
 elif salario>=500000.01 :
-  deducciones_anuales5=((salario-500000.00 )*0.30)+82500
-  deducciones_mensual_5=deducciones_anuales5/12
-  saldo_neto5=(salario-deducciones_anuales5) / 12 
-  print("salario  mensual luego de deducciones: ", saldo_neto5)
-  print("IR anual  : ", deducciones_anuales5)
-  print("IR mensual es: " , deducciones_mensual_5)
+     deducciones_anuales5 =((salario-500000.00 )*0.30)+82500
+     print("IR anual  : ", deducciones_anuales5)
+  
 else:
   ("nel perrro")
   
-  print("gracias")
+print("gracias")
